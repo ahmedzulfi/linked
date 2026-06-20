ab921ef0918e7d43164a536db9258b140470292f
Ahmed Zulfiqar <ahmedzulfiqar.dev@gmail.com>
2026-06-20 16:23:20 +0500

    Auto-detect Better Auth server baseURL on Vercel using production/deployment system env variables

--- lib/auth.ts
+++ lib/auth.ts
@@ -12,7 +12,13 @@ export const auth = betterAuth({
   secret:
     process.env.BETTER_AUTH_SECRET ||
     "linkedpage_local_secret_key_123456_better",
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL:
+    process.env.BETTER_AUTH_URL ||
+    (process.env.VERCEL_PROJECT_PRODUCTION_URL
+      ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+      : process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : "http://localhost:3000"),
   emailAndPassword: {
     enabled: true,
     autoSignIn: true,
